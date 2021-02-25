c4ca91d665ca1a6c80503ff2f94d780adab3508a
bennettscience <brian.bennett2@gmail.com>
2021-02-25 15:07:46 -0500

    change chat embed URL

--- app/assets/javascripts/yt-chat-embed.js
+++ app/assets/javascripts/yt-chat-embed.js
@@ -1,10 +1,10 @@
 window.onload = () => {
-    let wrapper = document.querySelector(`.chat-embed-wrapper`);  
-    if(!wrapper) return
+    let wrapper = document.querySelector(`.chat-embed-wrapper`);
+    if (!wrapper) return
     let videoId = wrapper.dataset.videoId;
-    if(!videoId) return
-    let frame = document.createElement(`iframe`);   
-    frame.src = `https://ecs-chat.web.app/chat/${videoId}`;   
-    frame.id = `chat-embed`;  
-    wrapper.appendChild(frame); 
+    if (!videoId) return
+    let frame = document.createElement(`iframe`);
+    frame.src = `https://ecs-chat.web.app/rooms/${videoId}`;
+    frame.id = `chat-embed`;
+    wrapper.appendChild(frame);
 }
